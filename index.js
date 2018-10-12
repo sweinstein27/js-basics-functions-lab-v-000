cba644eb61ea3ca07d3ba6012d9e99e8036bb347
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 17:43:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,16 @@ function distanceTravelledInFeet(start, end){
   return Math.abs(start - end)*264
 }
 
-function calculatesFarePrice(start, destination){
-  switch (Math.abs(destination-start) {
-    case <400:
-      return "This is on us."
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
 
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
 }
