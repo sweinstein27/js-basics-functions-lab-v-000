1a9261208f2e27c39ad13269af36ab7be203a9ce
Scott Weinstein <sweinstein27@gmail.com>
2018-10-12 17:31:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks() {
-  
+function distanceFromHqInBlocks(block) {
+  if (block > 42){
+    return block - 42
+  } else {
+    return 42 - block
+  }
 }
